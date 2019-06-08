c28d03d30fbdf016071137fdad6cdd1591c2f6bf
Josh Yolles <joshyolles@gmail.com>
2019-06-07 22:30:51 -0700

    Added heavy shotgun and some mk2 weapons

--- weapons-on-back/cl_weapons-on-back.lua
+++ weapons-on-back/cl_weapons-on-back.lua
@@ -12,31 +12,34 @@ local SETTINGS = {
     y_rotation = 165.0,
     z_rotation = 0.0,
     compatable_weapon_hashes = {
-        -- melee:
-        --["prop_golf_iron_01"] = 1141786504, -- positioning still needs work
-        ["w_me_bat"] = -1786099057,
-        ["prop_ld_jerrycan_01"] = 883325847,
-        -- assault rifles:
-        ["w_ar_carbinerifle"] = -2084633992,
-        ["w_ar_assaultrifle"] = -1074790547,
-        ["w_ar_specialcarbine"] = -1063057011,
-        ["w_ar_bullpuprifle"] = 2132975508,
-        ["w_ar_advancedrifle"] = -1357824103,
-        -- sub machine guns:
-        ["w_sb_microsmg"] = 324215364,
-        ["w_sb_assaultsmg"] = -270015777,
-        ["w_sb_smg"] = 736523883,
-        ["w_sb_gusenberg"] = 1627465347,
-        -- sniper rifles:
-        ["w_sr_sniperrifle"] = 100416529,
-        -- shotguns:
-        ["w_sg_assaultshotgun"] = -494615257,
-        ["w_sg_bullpupshotgun"] = -1654528753,
-        ["w_sg_pumpshotgun"] = 487013001,
-        ["w_ar_musket"] = -1466123874,
-        -- ["w_sg_sawnoff"] = 2017895192 don't show, maybe too small?
-        -- launchers:
-        ["w_lr_firework"] = 2138347493
+      -- melee:
+      --["prop_golf_iron_01"] = 1141786504, -- positioning still needs work
+      ["w_me_bat"] = -1786099057,
+      ["prop_ld_jerrycan_01"] = 883325847,
+      -- assault rifles:
+      ["w_ar_carbinerifle"] = -2084633992,
+      ["w_ar_carbineriflemk2"] = GetHashKey("WEAPON_CARBINERIFLE_Mk2"),
+      ["w_ar_assaultrifle"] = -1074790547,
+      ["w_ar_specialcarbine"] = -1063057011,
+      ["w_ar_bullpuprifle"] = 2132975508,
+      ["w_ar_advancedrifle"] = -1357824103,
+      -- sub machine guns:
+      ["w_sb_microsmg"] = 324215364,
+      ["w_sb_assaultsmg"] = -270015777,
+      ["w_sb_smg"] = 736523883,
+      ["w_sb_smgmk2"] = GetHashKey("WEAPON_SMGMk2"),
+      ["w_sb_gusenberg"] = 1627465347,
+      -- sniper rifles:
+      ["w_sr_sniperrifle"] = 100416529,
+      -- shotguns:
+      ["w_sg_assaultshotgun"] = -494615257,
+      ["w_sg_bullpupshotgun"] = -1654528753,
+      ["w_sg_pumpshotgun"] = 487013001,
+      ["w_ar_musket"] = -1466123874,
+      ["w_sg_heavyshotgun"] = GetHashKey("WEAPON_HEAVYSHOTGUN"),
+      -- ["w_sg_sawnoff"] = 2017895192 don't show, maybe too small?
+      -- launchers:
+      ["w_lr_firework"] = 2138347493
     }
 }
 
